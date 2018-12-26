85784a4a56ea631c17037b85bf7e578770884884
cevans87 <c.d.evans87@gmail.com>
2018-12-26 14:59:49 -0800

    Added pypi setup.{cfg,py} files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='atools',
+    version='0.0.0',
+    packages=['', 'atools'],
+    url='https://github.com/cevans87/atools',
+    license='mit',
+    author='cevans',
+    author_email='c.d.evans87@gmail.com',
+    description='Python 3.6+ async decorators and testing tools'
+)
