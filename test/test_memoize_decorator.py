746717a5d6249dca4c35ca08648524cdf3290b00
cevans87 <c.d.evans87@gmail.com>
2018-12-31 16:38:06 -0800

    Small cleanups in test_memoize_decorator.py

--- test/test_memoize_decorator.py
+++ test/test_memoize_decorator.py
@@ -3,7 +3,7 @@ from atools import memoize
 from atools.util import duration
 from typing import List
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import call, MagicMock, patch
 
 
 class TestMemoize(unittest.TestCase):
@@ -13,146 +13,141 @@ class TestMemoize(unittest.TestCase):
         self.loop = get_event_loop()
 
     def test_zero_args(self) -> None:
-        calls = set()
+        body = MagicMock()
 
         @memoize
         def foo() -> None:
-            self.assertNotIn(None, calls)
-            calls.add(None)
+            body()
 
         foo()
         foo()
-        self.assertEqual({None}, calls)
+        body.assert_called_once()
 
     def test_keyword_same_as_default(self) -> None:
-        calls = set()
+        body = MagicMock()
 
         @memoize
         def foo(bar: int, baz: int = 1) -> int:
-            self.assertNotIn((bar, baz), calls)
-            calls.add((bar, baz))
+            body(bar, baz)
+
             return bar + baz
 
         self.assertEqual(foo(1), 2)
         # noinspection PyArgumentEqualDefault
         self.assertEqual(foo(1, baz=1), 2)
-        self.assertEqual(calls, {(1, 1)})
+        body.assert_called_once_with(1, 1)
 
     def test_async(self) -> None:
-        calls = set()
+        body = MagicMock()
 
         @memoize
         async def foo(bar: int, baz: int = 1) -> int:
-            self.assertNotIn((bar, baz), calls)
-            calls.add((bar, baz))
+            body(bar, baz)
+
             return bar + baz
 
         self.assertEqual(self.loop.run_until_complete(foo(1)), 2)
         # noinspection PyArgumentEqualDefault
         self.assertEqual(self.loop.run_until_complete(foo(1, baz=1)), 2)
-        self.assertEqual(calls, {(1, 1)})
+        body.assert_called_once_with(1, 1)
 
     def test_size(self) -> None:
-        calls = set()
+        body = MagicMock()
 
         @memoize(size=1)
         def foo(bar) -> None:
-            self.assertNotIn(bar, calls)
-            calls.add(bar)
+            body(bar)
 
         foo(0)
         foo(1)
-        self.assertEqual({0, 1}, calls)
-        calls.remove(0)
+        body.assert_has_calls([call(0), call(1)], any_order=False)
+        body.reset_mock()
         foo(0)
-        self.assertEqual({0, 1}, calls)
+        body.assert_called_once_with(0)
 
     def test_sync_exception(self) -> None:
         class FooException(Exception):
             ...
 
-        calls = set()
+        body = MagicMock()
 
         @memoize
         def foo() -> None:
-            self.assertNotIn(None, calls)
-            calls.add(None)
+            body()
             raise FooException()
 
         for _ in range(2):
             with self.assertRaises(FooException):
                 foo()
 
+        body.asser_called_once()
+
     def test_async_exception(self) -> None:
         class FooException(Exception):
             ...
 
-        calls = set()
+        body = MagicMock()
 
         @memoize
         async def foo() -> None:
-            self.assertNotIn(None, calls)
-            calls.add(None)
+            body()
             raise FooException()
 
         for _ in range(2):
             with self.assertRaises(FooException):
                 self.loop.run_until_complete(foo())
 
+        body.asser_called_once()
+
     @patch('atools.memoize_decorator.time')
     def test_expire_current_call(self, m_time: MagicMock) -> None:
-        calls = set()
+        body = MagicMock()
 
         @memoize(expire='24h')
         def foo() -> None:
-            self.assertNotIn(None, calls)
-            calls.add(None)
+            body()
 
         m_time.return_value = 0.0
         foo()
-        self.assertEqual({None}, calls)
-
-        calls.remove(None)
         m_time.return_value = duration('23h59m59s')
         foo()
-        self.assertEqual(set(), calls)
+        body.assert_called_once()
 
         m_time.return_value = duration('24h1s')
         foo()
-        self.assertEqual({None}, calls)
+        self.assertEqual(body.call_count, 2)
 
     @patch('atools.memoize_decorator.time')
     def test_expire_old_call(self, m_time: MagicMock) -> None:
-        calls = set()
+        body = MagicMock()
 
         @memoize(expire='24h')
         def foo(bar: int) -> None:
-            self.assertNotIn(bar, calls)
-            calls.add(bar)
+            body(bar)
 
         m_time.return_value = 0.0
         foo(1)
-        self.assertEqual({1}, calls)
+        body.assert_called_once_with(1)
+        body.reset_mock()
 
-        calls.remove(1)
         m_time.return_value = duration('24h1s')
         foo(2)
-        self.assertEqual({2}, calls)
+        body.assert_called_once_with(2)
+        body.reset_mock()
 
         m_time.return_value = 0.0
         foo(1)
-        self.assertEqual({1, 2}, calls)
+        body.assert_called_once_with(1)
 
     def test_async_thundering_herd(self) -> None:
-        calls = set()
+        body = MagicMock()
 
         @memoize
         async def foo() -> None:
-            self.assertNotIn(None, calls)
-            calls.add(None)
+            body()
 
         self.loop.run_until_complete(gather(foo(), foo()))
-        self.assertEqual({None}, calls)
+        body.assert_called_once()
 
     def test_size_le_zero_raises(self) -> None:
         for size in [-1, 0]:
@@ -168,7 +163,7 @@ class TestMemoize(unittest.TestCase):
                 def foo() -> None:
                     ...
 
-    def test_args_overlaps_kwargs(self) -> None:
+    def test_args_overlaps_kwargs_raises(self) -> None:
 
         @memoize
         def foo(_bar: int) -> None:
@@ -178,14 +173,14 @@ class TestMemoize(unittest.TestCase):
             foo(1, _bar=1)
 
     def test_pass_unhashable_true_passes_calls(self) -> None:
-        calls = []  # a set won't hold our unhashable
+        body = MagicMock()
 
         @memoize(pass_unhashable=True)
         def foo(bar: List) -> None:
-            calls.append(bar)
+            body(bar)
 
         foo([])
-        self.assertEqual([[]], calls)
+        body.assert_called_once_with([])
 
     def test_pass_unhashable_false_raises(self) -> None:
         @memoize(pass_unhashable=False)
@@ -196,41 +191,41 @@ class TestMemoize(unittest.TestCase):
             foo([])
 
     def test_reset(self) -> None:
-        calls = set()
+        body = MagicMock()
 
         @memoize
         def foo() -> None:
-            calls.add(None)
+            body()
 
         foo()
-        self.assertEqual({None}, calls)
-        calls.remove(None)
+        body.assert_called_once()
+        body.reset_mock()
 
         foo.reset()
         foo()
-        self.assertEqual({None}, calls)
+        body.assert_called_once()
 
     # FIXME The following tests do not prove the safety of the 'thread_safe' flag. The difficulty
     #  deterministically producing race conditions spawned the idea of RaceMock to force race
     #  conditions outside a mutex lock. See https://github.com/cevans87/atools/projects/4
 
-    def test_thread_safe_false_does_not_lock(self) -> None:
+    @patch('atools.memoize_decorator.Lock', side_effect=None)
+    def test_thread_safe_false_does_not_lock(self, m_lock: MagicMock) -> None:
         @memoize(thread_safe=False)
         def foo() -> None:
             ...
 
-        with patch('atools.memoize_decorator.Lock', side_effect=None) as m_lock:
-            foo()
-            m_lock.assert_not_called()
+        foo()
+        m_lock.assert_not_called()
 
-    def test_thread_safe_true_locks(self) -> None:
+    @patch('atools.memoize_decorator.Lock', side_effect=None)
+    def test_thread_safe_true_locks(self, m_lock: MagicMock) -> None:
         @memoize(thread_safe=True)
         def foo() -> None:
             ...
 
-        with patch('atools.memoize_decorator.Lock', side_effect=None) as m_lock:
-            foo()
-            m_lock.assert_called()
+        foo()
+        m_lock.assert_called()
 
 
 if __name__ == '__main__':
